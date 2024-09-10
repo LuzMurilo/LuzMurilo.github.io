3003d066e6a41d82f0cd1067e800bb87701ebcc4
LuzMurilo <murilo.stucki@gmail.com>
2024-09-10 17:30:51 -0300

    started implementing about section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,15 +5,17 @@ function Hero() {
 
 
   return (
-    <section className={styles.container}>
+    <section className={styles.heroSection}>
+      <div className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Murilo</h1>
             <p className={styles.description}>I'm a Front-End Developer and also a Game Developer from Brazil that is always looking for the next challenge!</p>
             <a className={styles.contactBtn} href="mailto:murilo.stucki@gmail.com">Contact Me</a>
         </div>
         <img className={styles.heroImg} src={getImageURL("hero/heroImage.png")} alt="Hero image - my photo" />
-        <div className={styles.topBlur}></div>
-        <div className={styles.bottomBlur}></div>
+      </div>
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
     </section>
   )
 }
