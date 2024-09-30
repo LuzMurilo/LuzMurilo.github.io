8dafaa4ae71765b4c85b5a8132e91d172ec845c5
LuzMurilo <murilo.stucki@gmail.com>
2024-09-30 00:58:08 -0300

    getting projects data from json and new layout

--- src/components/ProjectsFull/ProjectsFull.tsx
+++ src/components/ProjectsFull/ProjectsFull.tsx
@@ -2,41 +2,63 @@ import { getImageURL } from "../../utils";
 import Button from "../Button/Button";
 import styles from "./ProjectsFull.module.css";
 
+import data from "../../data/projects.json";
+
 function ProjectsFull() {
-  return (
-    <div className={styles.projects}>
-        <h1 className={styles.title}>My Projects</h1>
-        <div className={styles.cardsContainer}>
-            <div className={styles.card}>
-                <h2 className={styles.cardTitle}>Project Aster</h2>
-                <img className={styles.cardImg} src={getImageURL("projects/aster.png")} alt="Aster screenshot" />
-                <p className={styles.cardDescription}> Here's a description of the project, especifing which tecnologies were used and what's the main idea</p>
-                <div className={styles.cardButtons}>
-                    <Button className={styles.codeBtn} external to="github.com">See the Code</Button>
-                    <Button className={styles.demoBtn} to="github.com">Explore the Demo</Button>
+    if (!data)
+    {
+        return <h1>Failed to get projects data!</h1>
+    }
+
+    var projects1: typeof data.projects = [];
+    var projects2: typeof data.projects = [];
+    for (let i = 0; i < data.projects.length; i++)
+    {
+        if (i < data.projects.length/2) projects1.push(data.projects[i]);
+        else projects2.push(data.projects[i]);
+    }
+
+    return (
+        <div className={styles.projects}>
+            <h1 className={styles.title}>My Projects</h1>
+            <div className={styles.columns}>
+                <div className={styles.cardsContainer}>
+                    {
+                        projects1.map((project, index) => {
+                            return (
+                                <div className={styles.card} key={index}>
+                                    <h2 className={styles.cardTitle}>{project.title}</h2>
+                                    <img className={styles.cardImg} src={getImageURL(project.imageSrc)} alt={`${project.title} image`} />
+                                    <div className={styles.cardDescription} dangerouslySetInnerHTML={{__html: project.description}}/>
+                                    <div className={styles.cardButtons}>
+                                        {project.reposirory && <Button className={styles.codeBtn} external to={project.reposirory}>See the Code</Button>}
+                                        {project.demo && <Button className={styles.demoBtn} to={project.demo}>Explore the Demo</Button>}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-            </div>
-            <div className={styles.card}>
-                <h2 className={styles.cardTitle}>Project Aster</h2>
-                <img className={styles.cardImg} src={getImageURL("projects/aster.png")} alt="Aster screenshot" />
-                <p className={styles.cardDescription}> Here's a description of the project, especifing which tecnologies were used and what's the main idea</p>
-                <div className={styles.cardButtons}>
-                    <Button className={styles.codeBtn} external to="github.com">See the Code</Button>
-                    <Button className={styles.demoBtn} to="github.com">Explore the Demo</Button>
-                </div>
-            </div>
-            <div className={styles.card}>
-                <h2 className={styles.cardTitle}>Project Aster</h2>
-                <img className={styles.cardImg} src={getImageURL("projects/aster.png")} alt="Aster screenshot" />
-                <p className={styles.cardDescription}> Here's a description of the project, especifing which tecnologies were used and what's the main idea</p>
-                <div className={styles.cardButtons}>
-                    <Button className={styles.codeBtn} external to="https://www.github.com">See the Code</Button>
-                    <Button className={styles.demoBtn} to="github.com">Explore the Demo</Button>
+                <div className={styles.cardsContainer}>
+                    {
+                        projects2.map((project, index) => {
+                            return (
+                                <div className={styles.card} key={index}>
+                                    <h2 className={styles.cardTitle}>{project.title}</h2>
+                                    <img className={styles.cardImg} src={getImageURL(project.imageSrc)} alt={`${project.title} image`} />
+                                    <div className={styles.cardDescription} dangerouslySetInnerHTML={{__html: project.description}}/>
+                                    <div className={styles.cardButtons}>
+                                        {project.reposirory && <Button className={styles.codeBtn} external to={project.reposirory}>See the Code</Button>}
+                                        {project.demo && <Button className={styles.demoBtn} to={project.demo}>Explore the Demo</Button>}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProjectsFull
