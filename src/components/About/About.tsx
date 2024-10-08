a83784859e39c2d0e64f267edf560463343a6c8d
LuzMurilo <murilo.stucki@gmail.com>
2024-10-08 13:03:10 -0300

    transfered hero and about info to json files

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,8 @@
 import { getImageURL } from "../../utils";
 import styles from "./About.module.css";
 
+import aboutData from "../../data/about.json";
+
 function About() {
   return (
     <section className={styles.about} id="about">
@@ -9,18 +11,10 @@ function About() {
             <div className={styles.content}>
                 <img className={styles.aboutImg} src={getImageURL("about/aboutImage.png")} alt="about image of Murilo" />
                 <ul className={styles.cardList}>
-                    <li className={styles.card}>
-                        <img className={styles.cardImg} src={getImageURL("about/cursorIcon.png")} alt="cursor icon" />
-                        <p  className={styles.cardText}> Some text about me, graduated in Computer Engineering and worked as a Game Developer and Web Developer</p>
-                    </li>
-                    <li className={styles.card}>
-                        <img className={styles.cardImg} src={getImageURL("about/serverIcon.png")} alt="cursor icon" />
-                        <p  className={styles.cardText}> Some text about me, graduated in Computer Engineering and worked as a Game Developer and Web Developer</p>
-                    </li>
-                    <li className={styles.card}>
-                        <img className={styles.cardImg} src={getImageURL("about/uiIcon.png")} alt="cursor icon" />
-                        <p  className={styles.cardText}> Some text about me, graduated in Computer Engineering and worked as a Game Developer and Web Developer</p>
-                    </li>
+                    {aboutData.paragraphs.map((paragraph, index) => <li className={styles.card} key={index}>
+                        <img className={styles.cardImg} src={getImageURL(paragraph.image)} alt="cursor icon" />
+                        <p  className={styles.cardText}>{paragraph.text}</p>
+                    </li>)}
                 </ul>
             </div>
         </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,6 +2,8 @@ import { getImageURL } from "../../utils"
 import Button from "../Button/Button"
 import styles from "./Hero.module.css"
 
+import about from "../../data/about.json";
+
 function Hero() {
 
 
@@ -10,7 +12,7 @@ function Hero() {
       <div className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Greetings!</h1>
-            <p className={styles.description}>I'm a Front-End and Game Developer from Brazil that is always looking for the next challenge!</p>
+            <p className={styles.description}>{about.summary}</p>
             <Button to="/contact" className={styles.contactBtn}>Contact Me</Button>
         </div>
         <img className={styles.heroImg} src={getImageURL("hero/heroImage.png")} alt="Hero image - my photo" />
