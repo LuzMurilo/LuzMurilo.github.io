695c6b1c09e4e5f4c7ee4828c14210ff7878fb9f
LuzMurilo <murilo.stucki@gmail.com>
2024-10-17 16:26:20 -0300

    replaced hard coded values from the navbar and hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,9 +11,9 @@ function Hero() {
     <section className={styles.heroSection} id="hero">
       <div className={styles.container}>
         <div className={styles.content}>
-            <h1 className={styles.title}>Greetings!</h1>
+            <h1 className={styles.title}>{about.greetings}</h1>
             <p className={styles.description}>{about.summary}</p>
-            <Button to="/contact" className={styles.contactBtn}>Contact Me</Button>
+            {about.heroButton && <Button to={about.heroButton.path} className={styles.contactBtn}>{about.heroButton.text}</Button>}
         </div>
         <img className={styles.heroImg} src={getImageURL("hero/heroImage.png")} alt="Hero image - my photo" />
       </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import { getImageURL } from "../../utils";
 import { useState } from "react";
 import SvgIcon from "../SVG/SvgIcon";
 
+import data from "../../data/navigation.json";
+
 const Navbar = () => {
 
     const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -11,29 +13,21 @@ const Navbar = () => {
   return (
     <>
         <nav className={styles.navbar} id="navbar">
-            <Link to="/" className={styles.title}>Murilo Luz Stucki</Link>
+            <Link to="/" className={styles.title}>{data.title}</Link>
             <div className={styles.menu}>
                 <ul className={`${styles.navList} ${!menuOpen && styles.hidden}`}
                     onClick={() => setMenuOpen(false)}>
-                    <li className={styles.navItem}>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li className={styles.navItem}>
-                        <NavLink to="/experiences">Experiences</NavLink>
-                    </li>
-                    <li className={styles.navItem}>
-                        <NavLink to="/projects">Projects</NavLink>
-                    </li>
-                    <li className={styles.navItem}>
-                        <NavLink to="/contact">Contact</NavLink>
-                    </li>
+                    {data.pages.map((page) => 
+                            <li className={styles.navItem} key={page.pageName}>
+                                <NavLink to={page.path}>{page.pageName}</NavLink>
+                            </li>
+                    )}
                     <li className={`${styles.navItem} ${styles.socials}`}>
-                        <a href="https://www.linkedin.com/in/luz-murilo/" target="_blank" className={styles.socialBtn}>
-                            <SvgIcon icon="linkedin" />
-                        </a>
-                        <a href="https://github.com/LuzMurilo" target="_blank" className={styles.socialBtn}>
-                            <SvgIcon icon="github" />
-                        </a>
+                        {data.socials.map((social) => 
+                            <a href={social.link} target="_blank" className={styles.socialBtn} key={social.id}>
+                                <SvgIcon icon={social.icon} />
+                            </a>
+                        )}
                     </li>
                 </ul>
                 <img className={styles.menuBtn} 
